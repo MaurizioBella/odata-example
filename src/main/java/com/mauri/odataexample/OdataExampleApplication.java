8405b70bda0dffed78896f7b1676923e8f631038
Mauri <mauriziobella@gmail.com>
2019-12-02 22:33:42 -0500

    first commit

---
+++ src/main/java/com/mauri/odataexample/OdataExampleApplication.java
@@ -0,0 +1,14 @@
+package com.mauri.odataexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class OdataExampleApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdataExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mauri/odataexample/OdataExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauri.odataexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdataExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
